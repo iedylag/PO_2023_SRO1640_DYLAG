f7fcb5bd6f11036ec8801ac3bcf026ac8ce3fa9f
iedylag <123324325+iedylag@users.noreply.github.com>
2023-10-22 13:57:11 +0200

    done 10

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -4,6 +4,11 @@ public class World {
     public static void main(String[] args) {
 
         System.out.println("system wystartował");
+        run(args);
         System.out.println("system zakończył działanie");
+    }
+
+    public static void run(String[] args) {
+        System.out.println("zwierzak idzie do przodu");
     }
 }

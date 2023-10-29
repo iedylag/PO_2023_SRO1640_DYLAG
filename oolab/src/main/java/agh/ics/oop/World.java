bcacf84ff0cfc49bd6a44b762ce70b4f873120cd
Inga Dylag <iedylag@outlook.com>
2023-10-29 13:27:33 +0100

    Vector2d objects added to main

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,6 +1,7 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
 
 public class World {
     public static void main(String[] args) {
@@ -9,6 +10,12 @@ public class World {
         MoveDirection[] directions = OptionsParser.parse(args);
         run(directions);
         System.out.println("Stop");
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 
     public static void run(MoveDirection[] directions) {

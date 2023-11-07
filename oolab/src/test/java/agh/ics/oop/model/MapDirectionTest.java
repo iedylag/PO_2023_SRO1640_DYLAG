599ff96ed16c9ba92daf6006f340d07ac7937b03
iedylag <123324325+iedylag@users.noreply.github.com>
2023-11-07 11:40:09 +0100

    Create MapDirectionTest.java

---
+++ oolab/src/test/java/agh/ics/oop/model/MapDirectionTest.java
@@ -0,0 +1,51 @@
+package agh.ics.oop.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+class MapDirectionTest {
+
+    @Test
+    void nextDirection(){
+        //given
+        MapDirection dirSouth = MapDirection.SOUTH;
+        MapDirection dirEast = MapDirection.EAST;
+        MapDirection dirWest = MapDirection.WEST;
+        MapDirection dirNorth = MapDirection.NORTH;
+
+        //when
+        MapDirection dirSouth2 = dirSouth.next();
+        MapDirection dirEast2 = dirEast.next();
+        MapDirection dirWest2 = dirWest.next();
+        MapDirection dirNorth2 = dirNorth.next();
+
+        //then
+        assertSame(MapDirection.WEST, dirSouth2);
+        assertSame(MapDirection.SOUTH, dirEast2);
+        assertSame(MapDirection.NORTH, dirWest2);
+        assertSame(MapDirection.EAST, dirNorth2);
+    }
+
+    @Test
+    void prevDirection(){
+        //given
+        MapDirection dirSouth = MapDirection.SOUTH;
+        MapDirection dirEast = MapDirection.EAST;
+        MapDirection dirWest = MapDirection.WEST;
+        MapDirection dirNorth = MapDirection.NORTH;
+
+        //when
+        MapDirection dirSouth2 = dirSouth.previous();
+        MapDirection dirEast2 = dirEast.previous();
+        MapDirection dirWest2 = dirWest.previous();
+        MapDirection dirNorth2 = dirNorth.previous();
+
+        //then
+        assertSame(MapDirection.EAST, dirSouth2);
+        assertSame(MapDirection.NORTH, dirEast2);
+        assertSame(MapDirection.SOUTH, dirWest2);
+        assertSame(MapDirection.WEST, dirNorth2);
+    }
+
+}
